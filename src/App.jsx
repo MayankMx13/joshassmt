deba162f7be5388f6733cdbe0a193fa2becc6234
MayankMx13 <mayankmishra3214@gmail.com>
2024-08-27 22:06:14 +0530

    render alive

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,34 @@ import Projects from "./routes/proj/projects"
 import Recommendation from "./routes/recommendations/recommendation"
 import Formpallet from "./components/form/formpallet"
 import Footer from "./routes/footer/footer"
+import { useEffect } from "react";
 
 
 
 function App() {
-  return (
-    <div className='app'>
-      <Navbar />
-      <Hero />
-      <Projects />
-      <Recommendation />
-      <Formpallet />
-      <Footer />
 
-    </div>
-  )
-}
 
-export default App
+  function App() {
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        fetch("https://json-server-azvl.onrender.com")
+          .then(data => console.log("Server is alive:", data))
+          .catch(error => console.error("Error keeping server alive:", error));
+      }, 50000);
+      return () => clearInterval(intervalId);
+    }, []);
+
+    return (
+      <div className='app'>
+        <Navbar />
+        <Hero />
+        <Projects />
+        <Recommendation />
+        <Formpallet />
+        <Footer />
+
+      </div>
+    )
+  }
+
+  export default App
